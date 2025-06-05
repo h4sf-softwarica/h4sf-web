1576a6d6706938053efbe3647afcc24ed8c3b2a0
devRajeshThapa <devrajeshthapa@gmail.com>
2025-06-05 18:00:51 +0545

    ip

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      protocol: 'wss', // secure websocket (matches https)
+      host: 'freezoscan.rajeshthapa69.com.np', // your domain name
+      port: 443,
+    }
   },
   plugins: [
     react(),
